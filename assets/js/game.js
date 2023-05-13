a05d9ba0b39db9f70ae415d642108317064a4c5e
hashim222 <aslamhashim19@gmail.com>
2023-05-13 19:23:19 +0000

    Added random image position generator

---
+++ assets/js/game.js
@@ -0,0 +1,24 @@
+const onloadFunction = () => {
+  let imageIDs = [
+    "redirect-to-play-game-image-btn-1",
+    "redirect-to-play-game-image-btn-2",
+    "redirect-to-play-game-image-btn-3",
+  ];
+
+  for (let i = 1; i <= imageIDs.length; i++) {
+    //First get the image height and width in a let
+    let element = document.getElementById(imageIDs[i - 1]);
+    let positionInfo = element.getBoundingClientRect();
+    let imgHeight = positionInfo.height;
+    let imgWidth = positionInfo.width;
+
+    let screenWidth = window.innerWidth;
+    let screenHeight = window.innerHeight;
+
+    let imgLeft = Math.floor(Math.random() * (screenWidth - imgWidth));
+    let imgTop = Math.floor(Math.random() * (screenHeight - imgHeight));
+
+    document.getElementById(imageIDs[i - 1]).style.top = imgTop + "px";
+    document.getElementById(imageIDs[i - 1]).style.left = imgLeft + "px";
+  }
+};

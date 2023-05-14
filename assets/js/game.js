45d09447c9808f752d361e483581db7b048cf434
Shane-Donlon <shane.donlon@live.com>
2023-05-14 13:15:10 +0100

    Feat add: Yoda game imported, show/hide animation grid added

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,3 +22,45 @@ const onloadFunction = () => {
     document.getElementById(imageIDs[i - 1]).style.left = imgLeft + "px";
   }
 };
+
+// toggles claslist to dispaly contnent in animcation for yoda words game
+
+yodaGameSpan = document.querySelector(".redirect-to-play-game-image-btn-1");
+yodaGrid = document.querySelector(".yoda-words-game-grid");
+yodaGameSpan.addEventListener("click", () => {
+  yodaGrid.classList.toggle("active");
+});
+
+// Yoda Game JS
+
+const yodaWords = "Doing great young padawan, you are!";
+
+var img = document.createElement("img");
+var src = document.getElementById("yoda-pic-1");
+
+img.setAttribute("class", "yoda-img");
+img.src = "assets/images/yoda-thumb-up.png";
+src.appendChild(img);
+
+var words = yodaWords.split(" ");
+
+random = words.sort(() => Math.floor(Math.random() * Math.floor(1)) - 1);
+
+for (let i = 0; i < random.length; i++) {
+  let newWord = document.createElement("span");
+  newWord.setAttribute("class", "yoda-wrong-words");
+  newWord.textContent = random[i];
+  document.getElementById("yoda-words-wrong").appendChild(newWord);
+}
+
+let button = document.querySelector("#submit");
+button.addEventListener("click", () => {
+  let answer = document.getElementById("answer").value;
+  console.log(answer);
+  if (answer === yodaWords) {
+    alert("CORRECT! Well done");
+    window.location.replace("game.html");
+  } else alert("Wrong answer, try again");
+});
+
+// End Yoda Game JS
